69e1156eb45539758c5434783134c88cc518f7aa
Ricardo Ibarra <coder10@gmail.com>
2019-03-01 13:41:47 -0800

    Adding iam roles for dynamoDB part 1

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 'use strict';
+const loremIpsum = require('lorem-ipsum');
+const output = loremIpsum();
 
 module.exports.hello = async (event, context) => {
   let body;
@@ -16,3 +18,12 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.lorem = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: output
+    })
+  }
+}
